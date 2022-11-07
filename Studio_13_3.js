7b757c94f7a10d91c37928a92ca0c6b6dfa22e62
Tan Gian Sen <No public email provided>
2022-11-07 22:27:26 +0800

    Changes made from Source Academy

--- Studio_13_3.js
+++ Studio_13_3.js
@@ -15,10 +15,36 @@ tail(Dylan)[0] = Betty;
 tail(Dylan)[1] = Cindy;
 tail(Cindy)[0] = Dylan;
 
+
+//for array extensions
+
+
 function get_suspects(all_students, custodian) {
     let grey = [custodian];
     let black = [];
     // You don't actually need an array for white
+    let queue = list();
+    function copy_arr(arr){
+        let res = [];
+        for(let i = 0; i< array_length(arr); i = i +1){
+            res[i] = arr[i];
+        }
+        return res;
+    }
+    
+    function enqueue(x) {
+        queue = append(queue, list(x));
+    }
+    
+    function dequeue() {
+        if (is_null(queue)) {
+            error("dequeue() cannot be done on a queue of size 0.");
+        }
+        
+        const ans = head(queue);
+        queue = tail(queue);
+        return ans;
+    }
 }
 
 get_suspects(students, custodian); // ["Betty", "Cindy", "Dylan"] in any order
