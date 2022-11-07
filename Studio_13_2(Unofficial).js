d51eec5e2cc52b58930ef0b09f805132ccc4f89a
Source Academy User <Tan Gian Sen>
2022-11-07 22:08:21 +0800

    Changes made from Source Academy

--- Studio_13_2(Unofficial).js
+++ Studio_13_2(Unofficial).js
@@ -2,10 +2,19 @@
 // FIRST PART //////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 function is_battery_pack_dying(log) {
-    function helper(...) {
-        
+    function helper(encountered,prev_A,Bcount) {
+        if (Bcount === 4){
+            return true;
+        }
+        else if (is_null(encountered)){
+            return false;
+        }
+        else{
+            return helper(tail(encountered), is_A(head(encountered)), !prev_A && is_B(head(encountered)) ? Bcount + 1 : 0);
+        }
+   
     }
-    return helper(...);
+    return helper(log,false,0);
 }
 
 function is_B(string) {
@@ -21,23 +30,30 @@ const logB = list("A", "B", "B", "B", "B", "B");
 const logC = list("B", "C", "B", "B", "B", "D");
 const logD = list("B", "A", "C", "B", "B", "B", "B", "A");
 
-display(is_battery_pack_dying(logA)); // false
-display(is_battery_pack_dying(logB)); // true
-display(is_battery_pack_dying(logC)); // false
-display(is_battery_pack_dying(logD)); // true
+//display(is_battery_pack_dying(logA)); // false
+//display(is_battery_pack_dying(logB)); // true
+//display(is_battery_pack_dying(logC)); // false
+//display(is_battery_pack_dying(logD)); // true
 
 const logE = build_list(x => "A", 5000); // stress test: recursive process fails
-display(is_battery_pack_dying(logE)); // false
+//display(is_battery_pack_dying(logE)); // false
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // SECOND PART /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 function get_dying_robot_indices(logs) {
-    function helper(...) {
-        
+    function helper(lst,ans,index){
+        if (is_null(lst)){
+            return ans;
+        }
+        const check = head(lst);
+        if(is_battery_pack_dying(check)){
+            ans = pair(index,ans);
+        }
+        return helper(tail(lst),ans,index + 1);
     }
-    return helper(...);
+    return helper(logs,null,0);
 }
 
 const logs = list(logA, logB, logC, logD);
@@ -53,4 +69,4 @@ function interleave_append(xs, ys) {
 }
 
 const big_logs = interleave_append(build_list(x => logA, 2000), build_list(x => logB, 2000)); // stress test
-display_list(get_dying_robot_indices(big_logs)); // every other index starting from 1
+//display_list(get_dying_robot_indices(big_logs)); // every other index starting from 1
